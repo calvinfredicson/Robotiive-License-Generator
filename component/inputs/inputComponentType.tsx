7fb9535cd010a332989b1f02f8fd53c10808ee23
Calvin Fredicson <calvin@iscoollab.com>
2022-10-22 15:54:31 +0800

    refactor: autocomplete structure

--- component/inputs/inputComponentType.tsx
+++ component/inputs/inputComponentType.tsx
@@ -1,44 +1,27 @@
 import { CheckBox, CheckBoxOutlineBlankOutlined } from "@material-ui/icons"
 import { Autocomplete, Checkbox, TextField } from "@mui/material"
-import { forwardRef } from "react"
-import { Controller } from "react-hook-form"
+import { ForwardedRef, forwardRef, useCallback, useMemo } from "react"
+import { Controller, ControllerRenderProps } from "react-hook-form"
 import { ComponentTypeMap } from "stringTemplates"
+import { ComponentType } from "types"
 
 interface ComponentTypeProps extends ReactHookForm.Controller {}
 
 export const InputComponentType = forwardRef<any, ComponentTypeProps>(
-  function ComponentTypeInput({ control, ...props }, ref) {
+  function ComponentTypeInput({ control }, ref) {
+    const componentType = useMemo(() => {
+      return Object.keys(ComponentTypeMap)
+        .sort()
+        .map((key) => ComponentTypeMap[key as unknown as ComponentType])
+    }, [])
+
     return (
       <Controller
-        render={({ field: { onChange, ...field } }) => (
-          <Autocomplete
-            {...props}
+        render={({ field: { ...field } }) => (
+          <ComponentAutocomplete
             {...field}
-            fullWidth
+            componentType={componentType}
             ref={ref}
-            multiple
-            options={Object.values(ComponentTypeMap)}
-            disableCloseOnSelect
-            getOptionLabel={(value) => value}
-            onChange={(_, value) => onChange([...value])}
-            renderOption={(props, option, { selected }) => (
-              <li {...props}>
-                <Checkbox
-                  icon={<CheckBoxOutlineBlankOutlined />}
-                  checkedIcon={<CheckBox />}
-                  style={{ marginRight: 8 }}
-                  checked={selected}
-                />
-                {option}
-              </li>
-            )}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Component Type"
-                placeholder="Component Type"
-              />
-            )}
           />
         )}
         name="componentType"
@@ -47,3 +30,55 @@ export const InputComponentType = forwardRef<any, ComponentTypeProps>(
     )
   }
 )
+
+interface ComponentAutocompleteProps
+  extends Omit<ControllerRenderProps, "ref"> {
+  ref: ForwardedRef<any>
+  componentType: string[]
+}
+
+const ComponentAutocomplete: React.FC<ComponentAutocompleteProps> = ({
+  ref,
+  componentType,
+  ...field
+}) => {
+  const onSelectChange = useCallback<
+    NonNullable<ControllerRenderProps["onChange"]>
+  >(
+    (value) => {
+      field.onChange([...value])
+    },
+    [field]
+  )
+
+  return (
+    <Autocomplete
+      {...field}
+      fullWidth
+      ref={ref}
+      multiple
+      options={componentType}
+      disableCloseOnSelect
+      getOptionLabel={(value) => value}
+      onChange={(_, value) => onSelectChange(value)}
+      renderOption={(props, option, { selected }) => (
+        <li {...props}>
+          <Checkbox
+            icon={<CheckBoxOutlineBlankOutlined />}
+            checkedIcon={<CheckBox />}
+            style={{ marginRight: 8 }}
+            checked={selected}
+          />
+          {option}
+        </li>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Component Type"
+          placeholder="Component Type"
+        />
+      )}
+    />
+  )
+}
